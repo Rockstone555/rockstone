dcc189bd7acfe6d4d62799c877e7d070eca2e591
Rockstone <rocky.zhang@live.com>
2018-06-29 15:39:48 +0800

    test from rocky dummy

---
+++ training.py
@@ -0,0 +1,14 @@
+import os
+os.mkdir('test')
+test = os.path.join(os.getcwd(),'test')
+test'/users/jeff/gitmen/projects/courses/python3/test'
+
+os.path.exists(test)
+
+Trueos.path.isdir(test)
+
+os.path.isfile(test)
+
+os.system('echo hello>test/hello.txt')
+os.makedirs('test/test2/level1/')
+os.makedirs('test/test2/level1/')
